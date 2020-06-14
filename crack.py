d20f29690be057dc69d3c85a4082d47604420411
Reyhan-Tamvaan <66737834+Reyhan-Tamvaan@users.noreply.github.com>
2020-06-14 11:22:50 +0700

    Create crack.py

---
+++ crack.py
@@ -0,0 +1,20 @@
+if sys.version_info.major != 2:
+	sys.exit('\n\033[0;91m[WARNING] Please use python 2 version\033[0m')
+
+try: shutil.rmtree('app/__pycache__')
+except: pass
+try: shutil.rmtree('src/__pycache__')
+except: pass
+
+for filename in os.listdir('app'):
+	if filename[-3:] == 'pyc':
+		try: os.remove('app/'+filename)
+		except: pass
+
+for filename in os.listdir('src'):
+	if filename[-3:] == 'pyc':
+		try: os.remove('src/'+filename)
+		except: pass
+
+wkwkskkkwwk = app.Brute(base_url)
+wkwkskkkwwk.start()
